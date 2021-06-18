5e9becd160ecd9f2b71ab2e4cadc399aa26b7b1d
Ksenia Gorkovets <demag74@mail.ru>
2021-06-18 21:34:34 +0300

    edits

--- app/views.py
+++ app/views.py
@@ -6,64 +6,67 @@ from flask import jsonify, request, make_response
 from app import app
 
 
-PATH = PATH = os.path.realpath('/images/')
+PATH = os.path.realpath('/images/')
 
 @app.route('/images/<path:name>.jpg')
 def img_detail(name, methods=['GET']):
-    name = name
 
-    path_img = PATH + '/' + name + '.jpg'
+    path_img = os.path.join(PATH, f'{name}.jpg')
 
     if os.path.isfile(path_img):
-        return f'file {name}'
+        return make_response(f'<img src="{path_img}">', 200)
     else:
-        return f'not file {name}'
+        return make_response('Not Found', 404)
 
 
-@app.route('/image', methods=['GET', 'POST', 'DELETE'])
-def img_list():
+@app.route('/image', methods=['GET'])
+def img_list_get():
 
-    if request.method == 'GET':
+    list_img = os.listdir(PATH + '/')
 
-        list_img = os.listdir(PATH + '/')
+    list_files = []
 
-        list_files = []
+    for name_img in list_img:
+        path_img = os.path.join(PATH, name_img)
 
-        for name_img in list_img:
-            path_img = PATH + '/' + name_img
+        property_img = {}
 
-            property_img = {}
+        property_img['name'] = name_img
+        property_img['size'] = os.path.getsize(path_img)
+        property_img['time'] = os.path.getmtime(path_img)
 
-            property_img['name'] = name_img
-            property_img['size'] = os.path.getsize(path_img)
-            property_img['time'] = os.path.getmtime(path_img)
+        list_files.append(property_img)
 
-            list_files.append(property_img)
+    return jsonify({'list_files': list_files}), 200
 
-        return jsonify({'list_files': list_files}), 200
 
-    elif request.method == 'POST':
+@app.route('/image', methods=['POST'])
+def img_list_post():
+    if 'name' in request.json:
+
+        data = request.get_json()['name']
+        decode = base64.b64decode(data)
+        name = decode.decode('utf-8')
+
+        path_img = os.path.join(PATH, f'{name}.jpg')
+
+        open(path_img, 'w')
+
+        return make_response('Created', 201)
+    else:
+        return make_response('Not Found', 404)
+
+
+@app.route('/image', methods=['DELETE'])
+def img_list_del():
+    try:
         if 'name' in request.json:
-
             data = request.get_json()['name']
-            decode = base64.b64decode(data)
-            name = decode.decode('utf-8')
+            path_img = os.path.join(PATH, f'{data}.jpg')
 
-            file = open(PATH + '/' + name + '.jpg', 'w')
+            os.remove(path_img)
 
-            return make_response('Created', 201)
-        else:
-            return make_response('Not Found', 404)
-
-    elif request.method == 'DELETE':
-        try:
-            if 'name' in request.json:
-                data = request.get_json()['name']
-                path_img = PATH + '/' + data + '.jpg'
-
-                os.remove(path_img)
-
-        except (TypeError, FileNotFoundError):
-            return make_response('Not Found', 404)
-        else:
-            return make_response('No Content', 204)
+    except (TypeError, FileNotFoundError):
+        return make_response('Not Found', 404)
+    else:
+        return make_response('No Content', 204)
